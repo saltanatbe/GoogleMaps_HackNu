c73790dcf5f9672f19ee940b9f6e341cc65af116
Saltanat Bekturgan <saltanat.bekturgan@nu.edu.kz>
2022-09-24 17:41:13 +0600

    xlsx

--- src/stores/xlsx.js
+++ src/stores/xlsx.js
@@ -1,12 +1,11 @@
 // var XLSX = require('xlsx');
-// var workbook = XLSX.readFile('hacknu-dev-list.xlsx');
+// var workbook = XLSX.readFile('hacknu-dev-data.xlsx');
 // var sheet_name_list = workbook.SheetNames;
 // console.log(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
 var XLSX = require('xlsx');
-//import * as XLSX from 'xlsx';
 var workbook = XLSX.readFile('hacknu-dev-data.xlsx');
 var sheet_name_list = workbook.SheetNames;
-var list = [];
+var data = [];
 sheet_name_list.forEach(function(y) {
     var worksheet = workbook.Sheets[y];
     var headers = {};
@@ -30,12 +29,13 @@ sheet_name_list.forEach(function(y) {
             continue;
         }
 
-        if(!list[row]) list[row]={};
-        list[row][headers[col]] = value;
+        if(!data[row]) data[row]={};
+        data[row][headers[col]] = value;
     }
     //drop those first two rows which are empty
-    list.shift();
-    list.shift();
-    console.log(list);
+    data.shift();
+    data.shift();
+    console.log(data);
 });
 
+module.exports = {data};
