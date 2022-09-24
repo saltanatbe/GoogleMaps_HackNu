35b6f7deb483d59d22de74e4748e96d613a7db79
asd4a10 <amanzholov000@gmail.com>
2022-09-24 13:42:56 +0600

    init

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarColor01"
+        aria-controls="navbarColor01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <router-link
+              class="nav-link"
+              :class="{ active: this.$route.path === '/' }"
+              to="/"
+              >Dynamic</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              class="nav-link"
+              to="/predefined"
+              :class="{ active: this.$route.path === '/predefined' }"
+              >Predefined</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,137 @@
+<script>
+import data from "@/stores/files.js";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { Loader } from "@googlemaps/js-api-loader";
+
+console.log("====================================");
+console.log("in about view");
+console.log("====================================");
+let index = 0;
+
+const apiOptions = {
+  apiKey: "AIzaSyAues8dw_usefVuVYKfmGAmPmBvPBqmCgY",
+  version: "beta",
+};
+
+const mapOptions = {
+  tilt: 0,
+  heading: 0,
+  zoom: 18,
+  center: {
+    lat: data.list[index].Latitude,
+    lng: data.list[index].Longitude,
+  },
+  altitude: data.list[index].Altitude,
+  mapId: "e1b4d53499a2fa30",
+};
+
+export default {
+  mounted() {
+    async function initMap() {
+      const mapDiv = document.getElementById("map-predef");
+      const apiLoader = new Loader(apiOptions);
+      await apiLoader.load();
+      return new google.maps.Map(mapDiv, mapOptions);
+    }
+
+    function initWebGLOverlayView(map) {
+      let scene, renderer, camera, loader;
+      const webGLOverlayView = new google.maps.WebGLOverlayView();
+
+      webGLOverlayView.onAdd = () => {
+        scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera();
+        const ambientLight = new THREE.AmbientLight(0xffffff, 0.75);
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 0.25);
+
+        directionalLight.position.set(0.5, -1, 0.5);
+        scene.add(ambientLight);
+        scene.add(directionalLight);
+
+        loader = new GLTFLoader();
+        loader.load("pin.gltf", (gltf) => {
+          gltf.scene.scale.set(5, 5, 5);
+          gltf.scene.rotation.x = (180 * Math.PI) / 180;
+          scene.add(gltf.scene);
+        });
+      };
+      webGLOverlayView.onContextRestored = ({ gl }) => {
+        renderer = new THREE.WebGLRenderer({
+          canvas: gl.canvas,
+          context: gl,
+          ...gl.getContextAttributes(),
+        });
+        renderer.autoClear = false;
+
+        let start = data.list[index].Timestamp;
+
+        loader.manager.onLoad = () => {
+          renderer.setAnimationLoop(() => {
+            start += 1000;
+            // console.log(start, data.list[index + 1].Timestamp);
+            if (start >= data.list[(index + 1) % data.list.length].Timestamp) {
+              index += 1;
+              if (index == data.list.length - 1) {
+                index = 0;
+                start = data.list[index].Timestamp;
+              }
+            }
+            mapOptions.center.lat = data.list[index].Latitude;
+            mapOptions.center.lng = data.list[index].Longitude;
+            mapOptions.altitude = data.list[index].Altitude;
+            // from tutorial
+            // map.moveCamera({
+            //   tilt: mapOptions.tilt,
+            //   heading: mapOptions.heading,
+            //   zoom: mapOptions.zoom,
+            // });
+            // if (mapOptions.tilt < 67.5) {
+            //   mapOptions.tilt += 0.5;
+            // } else if (mapOptions.heading <= 360) {
+            //   mapOptions.heading += 0.2;
+            // } else {
+            //   renderer.setAnimationLoop(null);
+            // }
+          });
+        };
+      };
+      webGLOverlayView.onDraw = ({ gl, transformer }) => {
+        const latLngAltitudeLiteral = {
+          lat: mapOptions.center.lat,
+          lng: mapOptions.center.lng,
+          altitude: mapOptions.altitude,
+        };
+
+        const matrix = transformer.fromLatLngAltitude(latLngAltitudeLiteral);
+        camera.projectionMatrix = new THREE.Matrix4().fromArray(matrix);
+
+        webGLOverlayView.requestRedraw();
+        renderer.render(scene, camera);
+        renderer.resetState();
+      };
+      webGLOverlayView.setMap(map);
+    }
+
+    (async () => {
+      console.log("====================================");
+      const map = await initMap();
+      initWebGLOverlayView(map);
+    })();
+  },
+};
+</script>
+
+<template>
+  <div>
+    <div id="map-predef" class="size"></div>
+  </div>
+</template>
+
+<style scoped>
+.size {
+  height: 600px;
+  /* width: 200px; */
+  background-color: aqua;
+}
+</style>
